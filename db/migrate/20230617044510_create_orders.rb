28b2fc6fbb4d7b6b1d79dd038789802215931b08
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-06-20 04:10:54 +0000

    注文機能作成

--- db/migrate/20230617044510_create_orders.rb
+++ db/migrate/20230617044510_create_orders.rb
@@ -1,6 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :id
+      t.interger :member_id
+      t.string :post_code
+      t.string :address
+      t.string :adressed_name
+      t.interger :status
+      t.interger :billing_price
+      t.interger :payment_method
+      t.interger :postage
+      t.datetime :created_at
+      t.datetime :updated_at
 
       t.timestamps
     end
--- db/migrate/20230617044544_create_order_details.rb
+++ db/migrate/20230617044544_create_order_details.rb
@@ -1,7 +1,14 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_details do |t|
-
+      t.integer :id
+      t.integer :item_id
+      t.integer :order_id
+      t.integer :price_tax
+      t.integer :amount
+      t.integer :status
+      t.datetime :created_at
+      t.datetime :updated_at
       t.timestamps
     end
   end
