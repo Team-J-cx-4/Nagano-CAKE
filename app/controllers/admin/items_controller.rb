b1cd398312ccc65f472040f85d401b2ea00b30b6
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-06-21 09:15:53 +0000

    ジャンル完了(未：レイアウト)、商品新規追加まで

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,18 +4,25 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image)
   end
 
 end
