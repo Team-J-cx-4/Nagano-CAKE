0b62ddeb1cc967a5baf05b969382ce2a57f58bf6
takedakazushi <ec2-user@ip-172-31-44-28.ap-northeast-1.compute.internal>
2023-06-24 07:49:24 +0000

    プルリク

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  
+
   before_action :authenticate_customer!
 
   def show
@@ -27,9 +27,9 @@ class Public::CustomersController < ApplicationController
 
   def out
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email, :is_deleted)
   end
