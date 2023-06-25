a09a37d106d02de281ea5dc36591df1dee53b795
aunnsann <127009562+aunnsann@users.noreply.github.com>
2023-06-25 10:16:50 +0000

    カート機能追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,31 +2,40 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @billing_price = 0
     @cart_items = current_customer.cart_items
   end
 
   def create
-    if CartItem.find_by(item.id)
-      @cart_item.quantity += @update_cart_item.quantity.to_i
-    else
-      @cart_item = CartItem.new(cart_item_params)
-      @cart_item.customer_id = current_customer.id
-      @cart_item.item_id = params[:item_id]
-      if @cart_item.save
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @new_cart_item = CartItem.new(cart_item_params)
+    if params[:cart_item][:amount] == ""
+      redirect_to item_path(params[:cart_item][:item_id])
+      return
+    end
+    if @cart_item
+      @cart_item.amount += @new_cart_item.amount
+      @cart_item.save
       redirect_to cart_items_path
+    else
+      @new_cart_item.customer_id = current_customer.id
+      if @new_cart_item.save
+        redirect_to cart_items_path
       else
-      render "customer/items/index"
+        @item = Item.find(params[:cart_item][:item_id])
+        render "/public/items/show"
       end
     end
   end
 
   def update
-    @cart_item.item_id = params[:item_id]
+    @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
-    @cart_item = Cartitem.find(params[:id])
+    @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
     flash[:alert] = "#{@cart_item.item.name}を削除しまし"
     redirect_to cart_items_path
