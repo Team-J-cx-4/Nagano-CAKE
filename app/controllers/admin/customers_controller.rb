086ce0a10e3c4bb42b4d909dbdb97ba2ef01afb0
Kenji Senda <ec2-user@ip-172-31-29-7.ap-northeast-1.compute.internal>
2023-06-26 07:17:06 +0000

    admin 会員詳細、編集画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,7 +17,7 @@ class Admin::CustomersController < ApplicationController
   def update
      @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to customer_path
+      redirect_to admin_customer_path
     else
       render 'edit'
     end
